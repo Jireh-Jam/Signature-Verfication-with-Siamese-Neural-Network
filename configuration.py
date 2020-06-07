249ce2517624d9a7813bf7d6e708f4f97c74cbce
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2020-06-07 23:06:49 +0200

    modify configuration parameter

--- configuration.py
+++ configuration.py
@@ -1,7 +1,7 @@
 configure_parameter= {"number_of_class":10,
                        "number_of_epoch":25,
-                       "image_height": 155,
-                       "image_width": 220,
+                       "image_height": 200,
+                       "image_width": 200,
                         "batch_size":128
                      }
 
